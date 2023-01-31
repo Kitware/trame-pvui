e740d8028426a9ace91b74e460945538a0c6f43b
Anne Haley <anne.haley@kitware.com>
2023-01-30 22:37:49 -0500

    Include example trame app

---
+++ example_trame_app/app/engine.py
@@ -0,0 +1,86 @@
+from .vtk_pipeline import VtkPipeline
+from pathlib import Path
+import logging
+
+from .filebrowser_functions import (
+    get_applicable_file_types,
+    get_dir_contents,
+    save_file,
+    open_file,
+)
+from .default_states import (
+    DEFAULT_COLOR_MAP,
+    DEFAULT_OPACITY_MAP,
+    DEFAULT_FILEBROWSER_STATE,
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+DATASET_PATH = Path("example_trame_app/data/skull.vti").absolute()
+
+
+class VtkPipelineEngine:
+    def initialize(self, server):
+        self.vtk_pipeline = VtkPipeline(DATASET_PATH)
+        state, ctrl = server.state, server.controller
+        state.trame__title = "View skull.vti"
+        self.state = state
+        self.ctrl = ctrl
+
+        @ctrl.set("get_render_window")
+        def get_render_window():
+            return self.vtk_pipeline.render_window
+
+
+class ColorMapperEngine(VtkPipelineEngine):
+    def initialize(self, server, **kwargs):
+        super().initialize(server)
+        state, ctrl = server.state, server.controller
+        state.colormap_points = DEFAULT_COLOR_MAP
+        state.opacity_points = DEFAULT_OPACITY_MAP
+        state.histogram_data = self.vtk_pipeline.get_histogram_data(buckets=10)
+
+        @state.change("colormap_points")
+        def update_colors(colormap_points, **kwargs):
+            self.vtk_pipeline.update_colors(colormap_points)
+            ctrl.view_update()
+
+        @state.change("opacity_points")
+        def update_opacity(opacity_points, **kwargs):
+            self.vtk_pipeline.update_opacity(opacity_points)
+            ctrl.view_update()
+
+        @ctrl.set("reset_colormap_points")
+        def reset_colormap_points(self):
+            self._server.state.colormap_points = DEFAULT_COLOR_MAP
+
+
+class FileBrowserEngine:
+    def initialize(self, server):
+        state, ctrl = server.state, server.controller
+        for key, value in DEFAULT_FILEBROWSER_STATE.items():
+            setattr(state, key, value)
+        state.current_local_dir_contents = get_dir_contents(state.current_local_dir)
+        state.current_remote_dir_contents = get_dir_contents(state.current_remote_dir)
+        state.file_types = get_applicable_file_types()
+        ctrl.save_file = save_file
+        ctrl.open_file = open_file
+
+        @state.change("current_local_dir")
+        def update_local_dir(current_local_dir, **kwargs):
+            state.current_local_dir_contents = get_dir_contents(current_local_dir)
+
+        @state.change("current_remote_dir")
+        def update_remote_dir(current_remote_dir, **kwargs):
+            state.current_remote_dir_contents = get_dir_contents(current_remote_dir)
+
+
+class WidgetTesterEngine:
+    def initialize(self, server):
+        ColorMapperEngine().initialize(server)
+        FileBrowserEngine().initialize(server)
+
+
+def initialize(server):
+    WidgetTesterEngine().initialize(server)
---
+++ example_trame_app/app/ui.py
@@ -0,0 +1,82 @@
+from trame.ui.vuetify import SinglePageWithDrawerLayout
+from trame.widgets import vuetify
+from trame.widgets import vtk
+from trame.app import get_server
+
+from colormapper.widget import ColormapEditor
+from filebrowser.widget import FileBrowser
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+
+def initialize(server):
+    ctrl = server.controller
+
+    with SinglePageWithDrawerLayout(server) as layout:
+        layout.title.set_text("Trame Widget Tester")
+
+        with layout.toolbar:
+            vuetify.VSpacer()
+            vuetify.VSwitch(
+                v_model="$vuetify.theme.dark",
+                hide_details=True,
+                label="Dark Mode",
+                dense=True,
+            )
+
+        with layout.drawer as drawer:
+            drawer.width = 450
+            with vuetify.VContainer(classes="pa-5"):
+                with vuetify.VTabs(grow=True, v_model=("tab", 0)):
+                    vuetify.VTab(children=["Color mapper"])
+                    vuetify.VTab(children=["File browser"])
+                with vuetify.VTabsItems(v_model=("tab", 0)):
+                    with vuetify.VTabItem():
+                        ColormapEditor(
+                            histogram_data=("histogram_data",),
+                            colors=("colormap_points",),
+                            opacities=("opacity_points",),
+                            update_colors="colormap_points = $event",
+                            update_opacities="opacity_points = $event",
+                        )
+                    with vuetify.VTabItem():
+                        FileBrowser(
+                            current_local_dir=("current_local_dir",),
+                            current_remote_dir=("current_remote_dir",),
+                            current_local_dir_contents=("current_local_dir_contents",),
+                            current_remote_dir_contents=(
+                                "current_remote_dir_contents",
+                            ),
+                            local_directories=("local_directories",),
+                            remote_directories=("remote_directories",),
+                            file_types=("file_types",),
+                            set_local_dir="current_local_dir = $event",
+                            set_remote_dir="current_remote_dir = $event",
+                            mode="Save",
+                            submit=ctrl.save_file,
+                        )
+                        FileBrowser(
+                            current_local_dir=("current_local_dir",),
+                            current_remote_dir=("current_remote_dir",),
+                            current_local_dir_contents=("current_local_dir_contents",),
+                            current_remote_dir_contents=(
+                                "current_remote_dir_contents",
+                            ),
+                            local_directories=("local_directories",),
+                            remote_directories=("remote_directories",),
+                            set_local_dir="current_local_dir = $event",
+                            set_remote_dir="current_remote_dir = $event",
+                            mode="Open",
+                            submit=ctrl.open_file,
+                        )
+
+        # Main content
+        with layout.content:
+            with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
+                html_view = vtk.VtkRemoteView(ctrl.get_render_window())
+                ctrl.on_server_ready.add(html_view.update)
+                ctrl.view_update = html_view.update
+
+        # Footer
+        layout.footer.hide()
