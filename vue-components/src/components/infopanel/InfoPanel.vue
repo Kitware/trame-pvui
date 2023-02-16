fa65002a2e800deea5a56501edf16e244fdf7156
Anne Haley <anne.haley@kitware.com>
2023-02-16 15:58:41 -0500

    Add start of InfoPanel widget

---
+++ vue-components/src/components/infopanel/InfoPanel.vue
@@ -0,0 +1,112 @@
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  props: {
+    fileProperties: {
+      type: Object,
+      required: true,
+    },
+    dataXMLString: {
+      type: String,
+      required: true,
+    },
+    dataStatistics: {
+      type: Object,
+      required: true,
+    },
+    dataArrays: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    const expanded = ref([0]);
+
+    return {
+      expanded,
+    };
+  },
+});
+</script>
+
+<template>
+  <v-card class="container" elevation="3">
+    <v-card-title class="dark-title">Information</v-card-title>
+    <v-expansion-panels v-model="expanded" accordion multiple>
+      <v-expansion-panel>
+        <v-expansion-panel-header class="font-weight-bold">
+          File Properties
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-simple-table dense>
+            <tbody>
+              <tr
+                v-for="[key, value] in Object.entries(fileProperties)"
+                :key="key"
+              >
+                <td>
+                  {{
+                    key.charAt(0).toUpperCase() +
+                    key.substr(1).replace('_', ' ')
+                  }}
+                </td>
+                <td>{{ value }}</td>
+              </tr>
+            </tbody>
+          </v-simple-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header class="font-weight-bold">
+          Data Grouping
+        </v-expansion-panel-header>
+        <v-expansion-panel-content> TODO </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header class="font-weight-bold">
+          Data Statistics
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-simple-table dense>
+            <tbody>
+              <tr
+                v-for="[key, value] in Object.entries(dataStatistics)"
+                :key="key"
+              >
+                <td>
+                  {{
+                    key.charAt(0).toUpperCase() +
+                    key.substr(1).replace('_', ' ')
+                  }}
+                </td>
+                <td>{{ value }}</td>
+              </tr>
+            </tbody>
+          </v-simple-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header class="font-weight-bold">
+          Data Arrays
+        </v-expansion-panel-header>
+        <v-expansion-panel-content> TODO </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-card>
+</template>
+
+<style scoped>
+.container {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 400px;
+  height: min(600px, 100%);
+  padding: 0;
+}
+.dark-title {
+  background-color: #444;
+  color: white;
+}
+</style>
---
+++ vue-components/src/tests/infopanel/App.vue
@@ -0,0 +1,79 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import components from '../../components';
+
+const { InfoPanel } = components;
+
+export default defineComponent({
+  name: 'HomePage',
+  components: { InfoPanel },
+  setup() {
+    return {
+      fileProperties: {
+        name: 'Inspire.png',
+        path: '/home/localUser/Documents/pv_examples/',
+      },
+      dataXMLString:
+        '<xml version="1.0">\
+        <IOSS type="vtkDataAssembly" version="1.0" id="1">\
+          <element_blocks id="0">\
+            <block_1 id="1" label="block_1">\
+              <dataset id="1"/>\
+            </block_1>\
+            <block_2 id="2" label="block_2">\
+              <dataset id="2"/>\
+            </block_2>\
+          </element_blocks>\
+          <node_sets id="1">\
+            <node_list_1 id="1"/>\
+            <node_list_100 id="100"/>\
+          </node_sets>\
+          <side_sets id="2">\
+            <surface_4 id="4"/>\
+          </side_sets>\
+        </IOSS>\
+      </xml>',
+      dataStatistics: {
+        num_datasets: 2,
+        type: 'Partitioned Dataset Collection',
+        cells_count: 7152,
+        points_count: 10088,
+        time_steps: 44,
+        current_time: 0.0042999,
+        time_range: [0, 0.0042999],
+        memory: '1.42578 MB',
+        bounds: [
+          [-121696, 9.08845],
+          [-360042, 8.43386],
+          [-19.3181, 11.6924],
+        ],
+      },
+      dataArrays: [
+        {
+          name: 'DistanceToCenter',
+          type: 'float',
+          range: [0, 8.66],
+        },
+        {
+          name: 'Polynomial',
+          type: 'float',
+          range: [1, 31.045],
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <v-app>
+      <info-panel
+        :fileProperties="fileProperties"
+        :dataXMLString="dataXMLString"
+        :dataStatistics="dataStatistics"
+        :dataArrays="dataArrays"
+      />
+    </v-app>
+  </div>
+</template>
