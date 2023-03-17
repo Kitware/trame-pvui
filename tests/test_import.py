4ddb29d8d99b305190065d926cc48ed329965ee8
Christos Tsolakis <christos.tsolakis@kitware.com>
2023-03-17 12:45:11 -0400

    chore(tests): update import statement in tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,7 +4,7 @@ def test_import_via_package():
 
 def test_import_via_trame():
     # For components only, the widgets are also importable via trame
-    from trame.widgets.trame_pvui import (  # noqa: F401
+    from trame.widgets.pvui import (  # noqa: F401
         Colormapper,
         FileBrowser,
         ServerBrowser,
